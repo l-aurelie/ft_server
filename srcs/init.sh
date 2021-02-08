ed0a965bd62a5b5e4e0d55b726035bdc885d7859
aurelie leconte <aurelieleconte.al@gmail.com>
2021-02-08 16:37:25 +0100

    pret a push

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,22 +1,19 @@
-#shabang
+#!/bin/bash
 
-#configure mysql
 service mysql start
 mysql -u root < /srcs/mysql_init
 
 ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
-
 rm -rf /etc/nginx/sites-enabled/default
 
 chown -R www-data:www-data /var/www/
 find /var/www// -type d -exec chmod 750 {} \;
 find /var/www// -type f -exec chmod 640 {} \;
-#chmod -R 644 /var/www/
-
 
 if [[ "$AUTOINDEX" == "off" ]]
 then
    sed -i 's/autoindex on/autoindex off/g' /etc/nginx/sites-available/localhost;
+cp /srcs/index.php /var/www/html/index.php;
 fi
 
 service nginx start
